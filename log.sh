dae6b91a5ff4ca12b4816a744d9abe148cc65b87
harshw2284 <harsbhagat0079@gmail.com>
2026-06-23 21:22:30 +0530

    updating shell workflow

--- log.sh
+++ log.sh
@@ -2,13 +2,13 @@
 
 read -r -p "Enter Number :" NUM
 
-if [ $NUM -gt 0 ]
+if [ "$NUM -gt 0" ]
 then 
 	echo "number is positive"
 	exit 1
 fi
 
-if [ $NUM -lt 0 ]
+if [ "$NUM -lt 0" ]
 then
 	echo "number is negative"
 	exit 1
